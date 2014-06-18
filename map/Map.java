12533a6a2b0604d2ffe80229fdc946526b5a8906
Billy Lau <billylau6@gmail.com>
2014-06-18 11:35:01 -0700

    Added layer selector for maps

--- map/Map.java
+++ map/Map.java
@@ -1,21 +1,63 @@
 package map;
 
 import java.awt.Color;
+import java.awt.List;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
+
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
 import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.Layer;
+import gov.nasa.worldwind.layers.LayerList;
 import gov.nasa.worldwind.layers.RenderableLayer;
 import gov.nasa.worldwind.render.Polyline;
+
 import javax.swing.JFrame;
 
 public class Map {
-	
+
 	public static void main(String[] args) {
 
 		//create a WorldWind main object
 		WorldWindowGLCanvas worldWindCanvas = new WorldWindowGLCanvas();
-		worldWindCanvas.setModel(new BasicModel());
+		BasicModel a=new BasicModel();
+		System.out.println(a.getLayers());
+
+		Set<String> abc=new HashSet<String>();
+		abc.add("Stars");
+		abc.add("Atmosphere");
+		//abc.add("NASA Blue Marble Image");
+		//abc.add("Blue Marble May 2004");
+		//abc.add("i-cubed Landsat");
+		abc.add("USDA NAIP");
+		abc.add("USDA NAIP USGS");
+		abc.add("MS Virtual Earth Aerial");
+		abc.add("Bing Imagery");
+		abc.add("USGS Topographic Maps 1:250K");
+		abc.add("USGS Topographic Maps 1:100K");
+		abc.add("USGS Topographic Maps 1:24K");
+		abc.add("USGS Urban Area Ortho");
+		abc.add("Political Boundaries");
+		abc.add("Open Street Map");
+		abc.add("Earth at Night");
+		//abc.add("Place Names");
+		abc.add("World Map");
+		//abc.add("Scale bar");
+		abc.add("Compass");
+
+		LayerList layerList=a.getLayers();
+
+		for (Layer x: layerList){
+			if (abc.contains(x.getName())){
+				layerList.remove(x);
+			}
+		}
+		a.setLayers(layerList);
+		System.out.println(a.getLayers());
+
+		worldWindCanvas.setModel(a);
 
 		//build Java swing interface
 		JFrame frame = new JFrame("World Wind");
@@ -23,7 +65,7 @@ public class Map {
 		frame.add(worldWindCanvas);
 		frame.setSize(800,600);
 		frame.setVisible(true);
-		
+
 		//create some "Position" to build a polyline
 		LinkedList<Position> list = new LinkedList<Position>();
 		for(int i = 0 ; i < 90 ; i++) {
@@ -32,18 +74,18 @@ public class Map {
 			//Maybe, there are some functions doing that in WWJ API...
 			list.add(Position.fromDegrees(i,0.0,i*20000));
 		}
-		
+
 		//create "Polyline" with list of "Position" and set color / thickness
 		Polyline polyline = new Polyline(list);
 		polyline.setColor(Color.RED);
 		polyline.setLineWidth(3.0);
-		
+
 		//create a layer and add Polyline
 		RenderableLayer layer = new RenderableLayer();
 		layer.addRenderable(polyline);
-		
+
 		//add layer to WorldWind
 		worldWindCanvas.getModel().getLayers().add(layer);
 	}
-	
+
 }
