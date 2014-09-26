de614e6ef5d2ae394a5d2423f0ec9410deb99706
Saeed Ghafghazi <saeedghaf@gmail.com>
2014-09-26 14:41:49 -0700

    commit

--- DataBase/Query.java
+++ DataBase/Query.java
@@ -344,7 +344,6 @@ public class Query extends Entity {
 		Iterator<JFrame> iterator =  resultFrames.iterator();
 		while (iterator.hasNext()){
 			JFrame target = iterator.next();
-			target.setVisible(false);
 			target.dispose();
 		}
 		return true;
@@ -355,7 +354,6 @@ public class Query extends Entity {
 		while (iterator.hasNext()){
 			JFrame target = iterator.next();
 			if (target.getName()==name){
-				target.setVisible(false);
 				target.dispose();
 				return true;
 			}
