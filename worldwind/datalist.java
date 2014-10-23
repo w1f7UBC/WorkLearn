73b65b343ea5e10492feb2892dc879a75ead6f0b
Newton <newtbelieve@hotmail.com>
2014-10-23 14:45:55 -0700

    Rewrote and fixed part of datalist

--- worldwind/datalist.java
+++ worldwind/datalist.java
@@ -1,5 +1,6 @@
 package worldwind;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -19,27 +20,95 @@ public class datalist {
 	
 	public void remove(Object entity, Object movingEntity){
 		Object[] objects = {entity, movingEntity};
-		if (structure.containsValue(objects)){
-			for (Entry<Double[], Object[]> target : structure.entrySet()){
-				if (target.getValue().equals(objects));
-					structure.remove(target);
+		for (Entry<Double[], Object[]> target : structure.entrySet()){
+			if (Arrays.equals(target.getValue(), objects)){
+				structure.remove(target.getKey());
+				return;
 			}
 		}
 	}
 	
 	public Double[] getKey(Object entity, Object movingEntity){
 		Object[] objects = {entity, movingEntity};
-		if (structure.containsValue(objects)){
-			for (Entry<Double[], Object[]> target : structure.entrySet()){
-				if (target.getValue().equals(objects));
-					return target.getKey();
+		for (Entry<Double[], Object[]> target : structure.entrySet()){
+			if (Arrays.equals(target.getValue(), objects)){
+				return target.getKey();
 			}
 		}
 		return null;
 	}
 	
-	public TreeMap<Double[], Object[]> getMap(){
-		return structure;
+	public Object[] getValue(double gScore, double fScore){
+		Object[] scores = {gScore, fScore};
+		for (Entry<Double[], Object[]> target : structure.entrySet()){
+			if (Arrays.equals(target.getKey(), scores)){
+				return target.getValue();
+			}
+		}
+		return null;
+	}
+	
+	public Boolean contains(Object entity, Object movingEntity){
+		Object[] objects = {entity, movingEntity};
+		for (Entry<Double[], Object[]> target : structure.entrySet()){
+			if (Arrays.equals(target.getValue(), objects)){
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public void printMap(){
+		for (Entry<Double[], Object[]> target : structure.entrySet()){
+			System.out.println(target.getKey()[0] + " " + target.getKey()[1] + " " + target.getValue()[0] + " " + target.getValue()[1]);
+		}
+	}
+	
+	public static void main(String args[]){
+		System.out.println("TEST");
+		datalist list= new datalist();
+		double a = 1;
+		double b = 2;
+		double c = 3;
+		double d = 4;
+		double e = 5;
+		
+		double aaa = 1;
+		double bbb = 2;
+		
+		String aa = "a";
+		String bb = "b";
+		String cc = "c";
+		String dd = "d";
+		String ee = "e";
+		
+		list.add(aa, aa, a, a);
+		list.add(cc, cc, c, c);
+		list.add(bb, bb, b, b);
+		list.add(ee, ee, e, e);
+		list.add(dd, dd, d, d);
+		
+		list.printMap();
+		System.out.println("Does list contain c, c " + list.contains(cc, cc));
+		System.out.println(" ");
+		list.remove(cc, cc);
+		
+		list.printMap();
+		
+		System.out.println("Does list contain c, c " + list.contains(cc, cc));
+		System.out.println(" ");
+		list.add(cc,  cc, c, c);
+		
+		list.printMap();
+		
+		System.out.println("Does list contain b, c " +list.contains(bb, cc));
+		System.out.println("Does list contain c, c " +list.contains(cc, cc));
+		
+		System.out.println("Get value for key 1, 1 " + list.getValue(a, a)[0] + list.getValue(a, a)[1]);
+		//System.out.println("Get value for key 1, 2 " + list.getValue(a, b)[0] + list.getValue(a, b)[1]);
+		
+		System.out.println("Get value for key 1, 1 (different object) " + list.getValue(aaa, aaa)[0] + list.getValue(aaa, aaa)[1]);
+		//System.out.println("Get value for key 1, 2 (different object) " + list.getValue(aaa, bbb)[0] + list.getValue(aaa, bbb)[1]);
 	}
 }
 
@@ -48,7 +117,12 @@ class scoreCompare implements Comparator<Double[]>{
 	@Override
 	public int compare(Double[] o1, Double[] o2) {
 		// TODO Auto-generated method stub
+		if (o1[0]<o2[0]){
+			return -1;
+		}
+		else if(o1[0]>o2[0]){
+			return 1;
+		}
 		return 0;
-	}
-	
+	}	
 }
